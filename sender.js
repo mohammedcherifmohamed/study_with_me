9303a880fa38b0828b4e38b61f8bb1bb8a15e23b
mohammed_cherif mohamed <mafsamarari@gmail.com>
2025-05-16 16:25:34 +0200

    Add files via upload

---
+++ sender.js
@@ -0,0 +1,35 @@
+//bot token
+var telegram_bot_id = "7579639557:AAFG3KSw27kKITFTjowtj8ylM6Ac49KW8bY";
+//chat id
+var chat_id = 5123629096;
+var u_name, email, message;
+var ready = function () {
+    u_name = document.getElementById("name").value;
+    email = document.getElementById("email").value;
+    message = document.getElementById("message").value;
+    message = "Name: " + u_name + "\nEmail: " + email + "\nMessage: " + message;
+};
+var sender = function () {
+    ready();
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api.telegram.org/bot" + telegram_bot_id + "/sendMessage",
+        "method": "POST",
+        "headers": {
+            "Content-Type": "application/json",
+            "cache-control": "no-cache"
+        },
+        "data": JSON.stringify({
+            "chat_id": chat_id,
+            "text": message
+        })
+    };
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("message").value = "";
+    return false;
+};
